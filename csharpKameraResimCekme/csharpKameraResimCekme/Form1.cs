29a937bf5203894266732597d9a97e9478ee0f66
hilal <62089994+ayperi-hilal@users.noreply.github.com>
2022-01-31 14:47:05 +0300

    Add files via upload

---
+++ csharpKameraResimCekme/csharpKameraResimCekme/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace csharpKameraResimCekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private FilterInfoCollection webcam;
+        private VideoCaptureDevice cam;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach(FilterInfo videocapturedevice in webcam)
+            {
+                comboBox1.Items.Add(videocapturedevice.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+        }
+        private void cam_NewFrame(object sender,NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bit;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(cam.IsRunning)
+            {
+                cam.Stop();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = pictureBox1.Image;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(cam.IsRunning)
+            {
+                cam.Stop();
+            }
+        }
+
+        
+    }
+}
